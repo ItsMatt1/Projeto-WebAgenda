f7fb1289444f92944206225d5c375840600fc47b
Matheus de Moura Rosa <rosa_matheus@discente.ufg.br>
2024-12-01 20:41:01 -0300

    Initial connection to mysql database

---
+++ WebAgenda/src/main/java/com/WebAgenda/WebAgenda/WebAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.WebAgenda.WebAgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAgendaApplication.class, args);
+	}
+
+}
---
+++ WebAgenda/src/test/java/com/WebAgenda/WebAgenda/WebAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebAgenda.WebAgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
