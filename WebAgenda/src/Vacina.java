b5b5e0ad5b8247c277e2b4e185e1973417dbef50
Matheus de Moura Rosa <rosa_matheus@discente.ufg.br>
2024-12-01 19:16:20 -0300

    Creating sample project

---
+++ WebAgenda/src/Vacina.java
@@ -0,0 +1,23 @@
+@Entity
+public class Vacina {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 60)
+    private String titulo;
+
+    @Column(length = 200)
+    private String descricao;
+
+    @Column(nullable = false)
+    private int doses;
+
+    @Column(nullable = false)
+    private int periodicidade;
+
+    @Column(nullable = false)
+    private int intervalo;
+
+    // Getters e Setters
+}
